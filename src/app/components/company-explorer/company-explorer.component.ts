d0d33143a0fb2965bbd974c769bf352b99f532e6
Gaetano Esposito <gaetanoesposito.exe@gmail.com>
2022-08-30 14:14:46 +0200

    finished sidebar v1

--- src/app/components/company-explorer/company-explorer.component.ts
+++ src/app/components/company-explorer/company-explorer.component.ts
@@ -1,4 +1,4 @@
-import { FlatTreeControl, NestedTreeControl } from '@angular/cdk/tree';
+import { NestedTreeControl } from '@angular/cdk/tree';
 import { Component } from '@angular/core';
 import { MatTreeNestedDataSource } from '@angular/material/tree';
 import { SideMenuNode } from './SideMenuNode.interface';
@@ -20,14 +20,6 @@ export class CompanyExplorerComponent {
   constructor(private sideMenuService: SideMenuService) {
     this.sideMenu.data = sideMenuService.getSideMenu();
   }
-
-  private _transformer = (node: SideMenuNode, level: number) => {
-    return {
-      expandable: !!node.children && node.children.length > 0,
-      name: node.name,
-      level: level,
-    };
-  };
 
   treeControl = new NestedTreeControl<SideMenuNode>((node) => node.children);
 
