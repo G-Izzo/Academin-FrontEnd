8805a6c6bb61102f7f226528074b6973cacd48a1
Gaetano Izzom <gaetanoizzo2001@gmail.com>
2022-09-05 15:28:22 +0200

    Eliminato course.model

--- src/app/services/xlsx.service.ts
+++ src/app/services/xlsx.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 const Excel = require('exceljs');
 import * as fs from 'file-saver';
+import { Course } from '../models/node.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +10,11 @@ export class XlsxService {
 
   constructor() { }
 
-  downloadExcel(){
+  downloadExcel(course:Course){
     let workbook = new Excel.Workbook();
-    let worksheet = workbook.addWorksheet("Employee Data");
-    let fname="Emp Data Sep 2020"
+    let worksheet = workbook.addWorksheet(`${course.name} ${new Date().getMonth()+1}-${new Date().getFullYear()}`);
+    let fname=`${course.name} ${new Date().getFullYear()}`;
+    
     workbook.xlsx.writeBuffer().then((data: BlobPart) => {
       let blob = new Blob([data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
       fs.saveAs(blob, fname+'-'+new Date().valueOf()+'.xlsx');
