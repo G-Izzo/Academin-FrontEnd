aa31272c6b7c57c3109b02cac8a38e01b74ca566
Gaetano Izzo <gaetanoizzo2001@gmail.com>
2022-09-05 16:55:49 +0200

    Inniettati nel appComponent il servizio XLXS e CourseService Aggiunto il metodo downloadExcel nel appComponent Generate le prime 3 trighe del excell Modificato Course Aggiunta interfaccia Lesson e lesson_student

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { Course } from './models/node.model';
+import { CourseService } from './services/course.service';
+import { XlsxService } from './services/xlsx.service';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,13 @@ export class AppComponent {
   @Output() selectedCourseEvent = new EventEmitter<Course>();
 
   title = 'Registro-FrontEnd';
+
+  constructor(private xlxs:XlsxService, private courseHttp:CourseService){}
+
+  //downloadExcel(id:number){
+  downloadExcel(){
+    let course:Course={id:1, name:"Azienda 1", starting_date:new Date("2022-08-03"), ending_date:new Date("2023-06-30")};
+    //let course:Course=this.courseHttp.getCourse(id);
+    this.xlxs.downloadExcel(course);
+  }
 }
--- src/app/components/company-explorer/mock_menu.ts
+++ src/app/components/company-explorer/mock_menu.ts
@@ -5,9 +5,9 @@ export const MOCK_MENU: Company[] = [
     id: 1,
     name: 'IBS',
     courses: [
-      { id: 1, name: 'IBS Academy' },
-      { id: 2, name: 'Minecraft Building Course' },
-      { id: 3, name: 'Corso 3' },
+      { id: 1, name: 'IBS Academy', starting_date:new Date("2020-08-20"), ending_date:new Date("2020-10-21")},
+      { id: 2, name: 'Minecraft Building Course', starting_date:new Date("2020-10-20"), ending_date:new Date("2021-03-21") },
+      { id: 3, name: 'Corso 3', starting_date:new Date("2022-06-20"), ending_date:new Date("2022-10-21") },
     ],
   },
   {
@@ -18,18 +18,18 @@ export const MOCK_MENU: Company[] = [
     id: 3,
     name: 'Azienda 3',
     courses: [
-      { id: 4, name: 'Corso 1' },
-      { id: 5, name: 'Corso 2' },
+      { id: 4, name: 'Corso 1', starting_date:new Date("2022-08-20"), ending_date:new Date("2022-12-21") },
+      { id: 5, name: 'Corso 2', starting_date:new Date("2023-01-20"), ending_date:new Date("2023-14-21") },
     ],
   },
   {
     id: 4,
     name: 'Azienda 4',
     courses: [
-      { id: 6, name: 'Corso 1' },
-      { id: 7, name: 'Corso 2' },
-      { id: 8, name: 'Corso 3' },
-      { id: 9, name: 'Corso 4' },
+      { id: 6, name: 'Corso 1', starting_date:new Date("2020-02-20"), ending_date:new Date("2020-10-21") },
+      { id: 7, name: 'Corso 2', starting_date:new Date("2021-11-20"), ending_date:new Date("2022-03-21") },
+      { id: 8, name: 'Corso 3', starting_date:new Date("2022-02-20"), ending_date:new Date("2020-07-21") },
+      { id: 9, name: 'Corso 4', starting_date:new Date("2023-08-20"), ending_date:new Date("2023-10-21") },
     ],
   },
 ];
