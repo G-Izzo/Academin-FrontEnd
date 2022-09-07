373c0d8544ffdd36f11c445dc495ae007335e0bb
Gaetano Izzo <gaetanoizzo2001@gmail.com>
2022-09-07 14:38:37 +0200

    Aggiunti metodi per la generazione e l'inserimento delle righe dei voti
    Resi facoltativi gli attributi CV_url e qualifications di Student
    
    Resi facoltativi gli attributi topics, join_time e exit_time di Lesson e lesson_student

--- src/app/models/lessons.model.ts
+++ src/app/models/lessons.model.ts
@@ -3,13 +3,13 @@ import { Student } from "./student.model";
 export interface Lesson{
     id:number;
     date:Date;
-    topics:String;
+    topics?:String;
     students:lesson_student[];
 }
 
-interface lesson_student{
-    join_time:Date;
-    exit_time:Date;
+export interface lesson_student{
+    join_time?:Date;
+    exit_time?:Date;
     daily_grade:number;
     student:Student;
 }
